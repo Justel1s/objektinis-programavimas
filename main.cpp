e6a5cbd780e534e085a3e51a8bfef45b4609391f
Justel1s <83273258+Justel1s@users.noreply.github.com>
2024-02-13 11:55:02 +0200

    2024-02-13

--- main.cpp
+++ main.cpp
@@ -7,24 +7,28 @@ struct duomenys{
 };
 int main()
 {
-    duomenys D[999];
+    duomenys D[INT_MAX];
     int kiekis, n, nd;
     double vidurkis, galutinis;
-    cout << "Pavardė        Vardas       Galutinis (Vid.)" << endl;
-    cout << "--------------------------------------------" << endl;
+    cout << "Pavardė        Vardas         Galutinis (Vid.)" << endl;
+    cout << "----------------------------------------------" << endl;
     cin >> kiekis;
     for(int i = 0; i < kiekis; i++)
     {
+        int med[INT_MAX];
         cin >> D[i].v >> D[i].p >> n;
         for(int j = 0; j < n; j++)
         {
             cin >> nd;
+            med[j] = nd;
             D[i].nd += nd;
         }
         cin >> D[i].egz;
+        med[n] = D[i].egz;
+        
         vidurkis = D[i].nd / n;
         galutinis = 0.4 * vidurkis + 0.6 * D[i].egz;
-        cout << D[i].p << D[i].v << galutinis << fixed << setprecision(2) << endl;
+        cout << left << setw(15) << D[i].p << left << setw(15) << D[i].v << left << setw(15) << setprecision(2) << fixed << galutinis << endl;
     }
     return 0;
 }
