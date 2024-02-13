379f67fcf60aac1df8f1c4eaa3bb47a8542cb282
Justel1s <83273258+Justel1s@users.noreply.github.com>
2024-02-13 14:28:40 +0200

    Pakeistas, jog galėčiau sąrašą pilnai surašyti 2024-02-13

--- main.cpp
+++ main.cpp
@@ -3,36 +3,45 @@ using namespace std;
 
 struct duomenys{
     string v, p;
-    int nd, egz;
+    int egz, dydis;
+    int nd[999];
 };
-double galutinis(duomenys *D, int n, int i){
-    double vidurkis = D[i].nd / n;
+
+int kiekis;
+duomenys D[999];
+
+void duomenuGavimas()
+{
+    cin >> kiekis;
+    if(kiekis == (int)kiekis){
+        for(int i = 0; i < kiekis; i++){
+        cin >> D[i].v >> D[i].p >> D[i].dydis;
+            for(int j = 0; j < D[i].dydis; j++){
+                cin >> D[i].nd[j];
+            }
+        cin >> D[i].egz;
+        }
+    }
+    else abort();
+    
+}
+double galutinis(int i){
+    double vidurkis = accumulate(D[i].nd, D[i].nd + D[i].dydis, 0.0) / D[i].dydis;
     return 0.4 * vidurkis + 0.6 * D[i].egz;
 }
-double mediana(int med[], int n){
-    n++;
-    sort(med, med+n);
-    return n % 2 == 0 ? (med[n/2 - 1] + med[n/2]) / 2.00 : med[n/2];
+double mediana(int i){
+    D[i].dydis++;
+    sort(D[i].nd, D[i].nd + D[i].dydis);
+    return D[i].dydis % 2 == 0 ? (D[i].nd[D[i].dydis/2 - 1] + D[i].nd[D[i].dydis/2]) / 2.00 : D[i].nd[D[i].dydis/2];
 }
 int main(){
-    duomenys D[999];
-    int kiekis, n, nd;
-    cin >> kiekis;
+    duomenuGavimas();
     for(int i = 0; i < kiekis; i++){
-        int med[999];
-        cin >> D[i].v >> D[i].p >> n;
-        for(int j = 0; j < n; j++){
-            cin >> nd;
-            med[j] = nd;
-            D[i].nd += nd;
-        }
-        cin >> D[i].egz;
-        med[n] = D[i].egz;
-        if (i == 0){
+        if(i == 0){
             cout << "Pavardė        Vardas         Galutinis (Vid.) / Galutinis (Med.)" << endl;
             cout << "-----------------------------------------------------------------" << endl;
         }
-        cout << left << setw(15) << D[i].p << left << setw(15) << D[i].v << left << setw(19) << setprecision(2) << fixed << galutinis(D, n, i) << setprecision(2) << fixed << mediana(med, n) << endl;
+        cout << left << setw(15) << D[i].p << left << setw(15) << D[i].v << left << setw(19) << setprecision(2) << fixed << galutinis(i) << setprecision(2) << fixed << mediana(i) << endl;
     }
     return 0;
 }
