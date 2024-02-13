6d95fdc1eef126197eb2b2443cf929eb3fb3961f
Justel1s <83273258+Justel1s@users.noreply.github.com>
2024-02-13 16:05:22 +0200

    Paskutinis v.pradinio commit 2024-02-13

--- main.cpp
+++ main.cpp
@@ -1,39 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Struktūra
 struct duomenys{
     string v, p;
     int egz, dydis;
     int nd[999];
 };
 
+//Globalūs kintamieji
 int kiekis;
 duomenys D[999];
 
-void duomenuGavimas()
-{
+//Duomenų įvedimo funkcija
+void duomenuGavimas(){
     cin >> kiekis;
-    if(kiekis == (int)kiekis){
-        for(int i = 0; i < kiekis; i++){
+    for(int i = 0; i < kiekis; i++){
         cin >> D[i].v >> D[i].p >> D[i].dydis;
-            for(int j = 0; j < D[i].dydis; j++){
-                cin >> D[i].nd[j];
-            }
-        cin >> D[i].egz;
+        for(int j = 0; j < D[i].dydis; j++){
+            cin >> D[i].nd[j];
         }
+        cin >> D[i].egz;
     }
-    else abort();
-    
 }
+//Vidurkio skaičiavimo funkcija
 double galutinis(int i){
     double vidurkis = accumulate(D[i].nd, D[i].nd + D[i].dydis, 0.0) / D[i].dydis;
     return 0.4 * vidurkis + 0.6 * D[i].egz;
 }
+//Medianos skaičiavimo funkcija
 double mediana(int i){
     D[i].dydis++;
+    D[i].nd[D[i].dydis-1] = D[i].egz;
     sort(D[i].nd, D[i].nd + D[i].dydis);
     return D[i].dydis % 2 == 0 ? (D[i].nd[D[i].dydis/2 - 1] + D[i].nd[D[i].dydis/2]) / 2.00 : D[i].nd[D[i].dydis/2];
 }
+//Pagrindinė funkcija
 int main(){
     duomenuGavimas();
     for(int i = 0; i < kiekis; i++){
