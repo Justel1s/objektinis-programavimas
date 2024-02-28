4a9a25f2128a191df655671c6bca5cfefef2aed3
Justel1s <83273258+Justel1s@users.noreply.github.com>
2024-02-28 13:17:23 +0200

    Vector atnaujinimas su try catch

--- vector.cpp
+++ vector.cpp
@@ -9,30 +9,53 @@ int main(){
     int pasirinkimas;
     while(true){
         cout << "1 - ranka, 2 - generuoti pazymius, 3 - generuoti ir pazymius ir studentu vardus, pavardes, 4 - skaityti iš failo, 5 - testi darba" << endl;        
-        if(cin>>pasirinkimas){
-            if(pasirinkimas == 1) variantas1(D);
-            if(pasirinkimas == 2) variantas2(D);
-            if(pasirinkimas == 3) variantas3(D);
-            if(pasirinkimas == 4) {
-                auto start = chrono::high_resolution_clock::now();
-                //Laiko skaiciavimo pradzia
-                variantas4(D);
-                auto end = chrono::high_resolution_clock::now();
-                auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
-                cout << "Time taken to read from file: " << duration.count() << " milliseconds" << endl;
+        while(!(cin>>pasirinkimas) || pasirinkimas < 1 || pasirinkimas > 5){
+            try{
+                throw runtime_error("Netinkama isvestis\n");
             }
-            if(pasirinkimas == 5) break;
+            catch (const runtime_error &e){
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				cout << e.what();
+                    cout << "1 - ranka, 2 - generuoti pazymius, 3 - generuoti ir pazymius ir studentu vardus, pavardes, 4 - skaityti iš failo, 5 - testi darba" << endl;        
+
+
+            }
         }
-        else {cout << "KLAIDA" << endl; break;}
+        if(pasirinkimas == 1) variantas1(D);
+        if(pasirinkimas == 2) variantas2(D);
+        if(pasirinkimas == 3) variantas3(D);
+        if(pasirinkimas == 4) {
+            auto start = chrono::high_resolution_clock::now();
+            //Laiko skaiciavimo pradzia
+            variantas4(D);
+            auto end = chrono::high_resolution_clock::now();
+            auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+            cout << "Time taken to read from file: " << duration.count() << " milliseconds" << endl;
+        }
+        if(pasirinkimas == 5) break;
     }
-    cout << "Rikiavimas pagal: 1 - varda, 2 - pavarde, 3 - galutini, 4 - mediana" << endl;
-    if(cin>>pasirinkimas){
+    
+    while(true){
+        cout << "Rikiavimas pagal: 1 - varda, 2 - pavarde, 3 - galutini, 4 - mediana" << endl;
+        while(!(cin>>pasirinkimas) || pasirinkimas < 1 || pasirinkimas > 4){
+            try{
+                throw runtime_error("Netinkama ivestis\n");
+            }
+            catch (const runtime_error &e){
+                cin.clear();
+                cout << e.what();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                cout << "Rikiavimas pagal: 1 - varda, 2 - pavarde, 3 - galutini, 4 - mediana" << endl;
+            }
             pasirinkimas = 1;
             if(pasirinkimas == 1) sort(D.begin(), D.end(), palyginimas1);
             if(pasirinkimas == 2) sort(D.begin(), D.end(), palyginimas2);
             if(pasirinkimas == 3) sort(D.begin(), D.end(), palyginimas3);
             if(pasirinkimas == 4) sort(D.begin(), D.end(), palyginimas4);
         }
+    }
+    
     for(int i = 0; i < D.size(); i++){
         if(i == 0){
             cout << "Pavardė        Vardas         Galutinis (Vid.) / Galutinis (Med.)" << endl;
